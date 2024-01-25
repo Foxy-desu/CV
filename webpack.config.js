b83b108acc5a30846d8b3be254734f29e33ff5a7
Alexandra <iammeduza@gmail.com>
2024-01-25 15:33:31 +0300

    feat: add necessary modules and configurations to the project

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js'
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: 'src/assets', to: 'assets'}
+      ]
+    })
+  ],
+  mode: 'development'
+};
