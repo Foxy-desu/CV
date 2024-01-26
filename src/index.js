c71697a57ea55dbbb5368d0966cb52d7170c447e
Alexandra <iammeduza@gmail.com>
2024-01-26 17:53:49 +0300

    feat: add  modules to the entry point

--- src/index.js
+++ src/index.js
@@ -7,13 +7,23 @@ import { renderMain } from "./js-modules/main";
 import { renderAsideBlocks } from "./js-modules/aside-blocks";
 import { renderContacts } from "./js-modules/contacts-section";
 import { renderEduSection } from "./js-modules/edu-section";
+import { renderSkillsSection } from "./js-modules/skills-section";
+import { renderLangSection } from "./js-modules/lang-section";
+import { renderAboutSection } from "./js-modules/about-section";
+import { renderExp } from "./js-modules/exp-section";
+import { renderFooter } from "./js-modules/footer";
 
-const education = renderEduSection(cvData)
+const experience = renderExp(cvData);
+const about = renderAboutSection(cvData);
+const langusges = renderLangSection(cvData);
+const skills = renderSkillsSection(cvData);
+const education = renderEduSection(cvData);
 const contacts = renderContacts(cvData);
-const asideBlocks = renderAsideBlocks(contacts, education);
-const main = renderMain('content main')
-const aside = renderAside(asideBlocks)
+const asideBlocks = renderAsideBlocks(contacts, education, skills, langusges);
+const main = renderMain(about, experience);
+const aside = renderAside(asideBlocks);
+const footer = renderFooter(cvData)
 const mainWrap =  renderWrap(aside, main);
 const header = renderHeader(cvData);
-const page = renderPage(header, mainWrap);
+const page = renderPage(header, mainWrap, footer);
 document.body.insertAdjacentHTML("afterbegin", page);
