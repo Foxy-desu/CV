c989af6d3e076a34842cefb67103604f3153618a
Alexandra <iammeduza@gmail.com>
2024-01-27 21:42:08 +0300

    fix: apply better implementation for the block

--- src/js-modules/exp-section.js
+++ src/js-modules/exp-section.js
@@ -1,17 +1,22 @@
 function renderExp(data) {
     const exp = data.mainBlock.experience;
     const expElems = exp.map((entry)=> {
+        const name = entry.name.split(' ').map((word)=>{
+            return `<span>${word}<br></span>`
+        }).join('\n')
         return (
             `<li class="experience__exp-entry">
-                <div class="exp-entry__img-wrap"><img class="exp-entry__img" src="${entry.thumb}" alt="${entry.name}"/></div>
-                <p class="exp-entry__name">${entry.name}</p>
-                <a class="exp-entry__link">${entry.src}</a>
+                <a class="exp-entry__link" href="${entry.src}">
+                <div class="exp-entry__img-wrap">
+                    <img class="exp-entry__img" width="70" height="70" src="${entry.thumb}" alt="${entry.name}"/>
+                </div>
+                <p class="exp-entry__text">${name}</p></a>
             </li>`
         )
     })
     return (
         `<section class="experience section">
-            <h2 class="experience__heading content-heading">experience</h2>
+            <h2 class="experience__heading content-heading">projects</h2>
             <ul class="experience__list">
                 ${expElems.join('\n')}
             </ul>
