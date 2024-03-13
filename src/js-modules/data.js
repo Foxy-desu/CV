cccd881f4a03cc3c7bc557ee1c8b6bcc4f798957
Foxy-desu <87661341+Foxy-desu@users.noreply.github.com>
2024-03-14 02:03:45 +0300

    fix: edit telegram info in data.js

--- src/js-modules/data.js
+++ src/js-modules/data.js
@@ -14,7 +14,7 @@ const cvData = {
         contacts: {
             phone: '+79893544196',
             mail: 'iammeduza@gmail.com',
-            telegram: 'Pincalsthedeer'
+            telegram: 'Pinkalsthedeer'
         },
         education: [
             {
